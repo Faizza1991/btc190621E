34b857451bcbfb0e58303c49e8cd3772d548b50a
Farzina Rashid <Kakter45@gmail.com>
2019-11-14 14:36:18 -0500

    heloo

---
+++ New Java Project/src/com/bit/test/Calculator.java
@@ -0,0 +1,14 @@
+package com.bit.test;
+
+public class Calculator {
+	
+	public static void main (String []args) {
+		
+		Demo d= new Demo();
+		
+	}
+
+}
+// i have to start my hub machine using this command "java-jar"
+// location of your hub machine 
+//t
---
+++ New Java Project/src/com/bit/test/Demo.java
@@ -0,0 +1,15 @@
+package com.bit.test;
+
+public class Demo {
+ 
+	// i want to add 5 and 8
+	
+	void add()
+	{
+		System.out.println(5+8);
+		
+	 }
+	
+	
+
+}
